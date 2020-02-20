8c6f6b984bb91033671be11901a96cf6ddb042e6
aru oreki <aruoreki@arudemac.local>
2020-02-19 22:33:28 -0800

    Initial Commit

---
+++ TodoList/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TodoList
+//
+//  Created by aru oreki on 2020/2/19.
+//  Copyright © 2020 aru oreki. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ TodoListTests/TodoListTests.m
@@ -0,0 +1,37 @@
+//
+//  TodoListTests.m
+//  TodoListTests
+//
+//  Created by aru oreki on 2020/2/19.
+//  Copyright © 2020 aru oreki. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TodoListTests : XCTestCase
+
+@end
+
+@implementation TodoListTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
